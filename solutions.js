b243988b6b57d625e628fdc35893363bd51cbe9d
Mr Robin Ahmed <mdrobinahmed57898@gmail.com>
2026-04-17 17:34:29 +0600

    solve: problem 2 - fizzbuzz

--- solutions.js
+++ solutions.js
@@ -1,16 +1,32 @@
 //* solutions Problem 1 — Reverse a String
 
-function   reverseString(hallo){
-let result = ""
-for (let i = hallo.length -1; i>= 0; i--){
-result = result + hallo[i]
-}
-    return result
-}
-console.log(reverseString("hallo"))
+// function   reverseString(hallo){
+// let result = ""
+// for (let i = hallo.length -1; i>= 0; i--){
+// result = result + hallo[i]
+// }
+//     return result
+// }
+// console.log(reverseString("hallo"))
 
 //*git commit -m "solve: problem 1 - reverse string"
 
 
 //* solutions Problem 2 — FizzBuzz
- 
+
+ function fizzBuzz(n) {
+   for (let i = 1; i <= n; i++){
+  if (i % 3 === 0 && i % 5 === 0){
+    console.log("FizzBuzz");
+  } else if(i % 3=== 0 ){
+    console.log("Fizz");
+  } else if(i % 5=== 0 ){
+    console.log("Buzz");
+   }else {
+    console.log(i)
+   }
+  }
+ } 
+fizzBuzz(15)
+
+
