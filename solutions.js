51acf1c23eb346a09a53d12ef491416bbefafc9c
Mr Robin Ahmed <mdrobinahmed57898@gmail.com>
2026-04-17 23:48:46 +0600

    Solve find largest number in array using loop

--- solutions.js
+++ solutions.js
@@ -1,5 +1,4 @@
 //* solutions Problem 1 — Reverse a String
-
 // function   reverseString(hallo){
 // let result = ""
 // for (let i = hallo.length -1; i>= 0; i--){
@@ -9,24 +8,32 @@
 // }
 // console.log(reverseString("hallo"))
 
-//*git commit -m "solve: problem 1 - reverse string"
-
 
 //* solutions Problem 2 — FizzBuzz
+//  function fizzBuzz(n) {
+//    for (let i = 1; i <= n; i++){
+//   if (i % 3 === 0 && i % 5 === 0){
+//     console.log("FizzBuzz");
+//   } else if(i % 3=== 0 ){
+//     console.log("Fizz");
+//   } else if(i % 5=== 0 ){
+//     console.log("Buzz");
+//    }else {
+//     console.log(i)
+//    }  }
+//  } 
+// fizzBuzz(15)
 
- function fizzBuzz(n) {
-   for (let i = 1; i <= n; i++){
-  if (i % 3 === 0 && i % 5 === 0){
-    console.log("FizzBuzz");
-  } else if(i % 3=== 0 ){
-    console.log("Fizz");
-  } else if(i % 5=== 0 ){
-    console.log("Buzz");
-   }else {
-    console.log(i)
-   }
+
+//* solutions Problem 3 — Find the Largest Number in an Array
+function findLargest (num) {
+  let  max = num[0];
+
+  for (let i = 1; i < num.length; i++ ){
+  if(num [i] > max){
+  max = num[i];
+    }
   }
- } 
-fizzBuzz(15)
-
-
+ return max;
+}
+console.log(findLargest([3, 7, 2, 9, 5])); 
