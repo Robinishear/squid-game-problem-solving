4dca14d1dab796ac7c2124b2b89c538a4fa72a2b
Mr Robin Ahmed <mdrobinahmed57898@gmail.com>
2026-04-18 01:43:42 +0600

    Add palindrome check solution using string reverse

--- solutions.js
+++ solutions.js
@@ -1,28 +1,28 @@
 //* solutions Problem 1 — Reverse a String
-// function   reverseString(hallo){
-// let result = ""
-// for (let i = hallo.length -1; i>= 0; i--){
-// result = result + hallo[i]
-// }
-//     return result
-// }
-// console.log(reverseString("hallo"))
+function   reverseString(hallo){
+let result = ""
+for (let i = hallo.length -1; i>= 0; i--){
+result = result + hallo[i]
+}
+    return result
+}
+console.log(reverseString("hallo"))
 
 
 //* solutions Problem 2 — FizzBuzz
-//  function fizzBuzz(n) {
-//    for (let i = 1; i <= n; i++){
-//   if (i % 3 === 0 && i % 5 === 0){
-//     console.log("FizzBuzz");
-//   } else if(i % 3=== 0 ){
-//     console.log("Fizz");
-//   } else if(i % 5=== 0 ){
-//     console.log("Buzz");
-//    }else {
-//     console.log(i)
-//    }  }
-//  } 
-// fizzBuzz(15)
+ function fizzBuzz(n) {
+   for (let i = 1; i <= n; i++){
+  if (i % 3 === 0 && i % 5 === 0){
+    console.log("FizzBuzz");
+  } else if(i % 3=== 0 ){
+    console.log("Fizz");
+  } else if(i % 5=== 0 ){
+    console.log("Buzz");
+   }else {
+    console.log(i)
+   }  }
+ } 
+fizzBuzz(15)
 
 
 //* solutions Problem 3 — Find the Largest Number in an Array
@@ -37,3 +37,17 @@ function findLargest (num) {
  return max;
 }
 console.log(findLargest([3, 7, 2, 9, 5])); 
+
+
+//* solutions Problem 4 — Check for Palindrome
+function isPalindrome (s){
+  let palindrome  = s.split('').reverse().join('');
+  if  (s === palindrome){
+  return true
+  } else {
+    return false;
+  }
+}
+
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("hallo"));
